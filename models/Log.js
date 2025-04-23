c7e623a9586cc5b0fe4ab77d4c61bfeb07530e35
Rey <115681255+raihants@users.noreply.github.com>
2025-04-23 10:11:37 +0700

    Update Log.js

--- models/Log.js
+++ models/Log.js
@@ -1,7 +1,9 @@
 const db = require('../config/db');
 
 const Log = {
-  getAll: (cb) => db.query('SELECT * FROM logs', cb),
+  getAll: async () => {
+    const [rows] = await db.query('SELECT * FROM logs');
+    return rows;
+  },
 };
-
 module.exports = Log;
