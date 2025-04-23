734bea3c4a248b554dac6316a5fabf1ec32334c7
Rey <115681255+raihants@users.noreply.github.com>
2025-04-23 10:16:19 +0700

    Update projectController.js

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,12 +1,13 @@
 const Project = require('../models/Project');
 
-exports.getAllProjects = (req, res) => {
+exports.getAllProjects = async (req, res) => {
     console.log('User from token', req.user);
 
-  try {
-    const data = await Project.getAll();
-    res.json(data);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+    try {
+        const data = await Project.getAll();
+        res.json(data);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 };
+
